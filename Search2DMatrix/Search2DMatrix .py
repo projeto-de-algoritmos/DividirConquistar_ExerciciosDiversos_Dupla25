237da369903b20baefebe363073b5ad6830851cb
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2023-06-15 09:20:00 -0300

    Update Search2DMatrix .py

--- Search2DMatrix/Search2DMatrix .py
+++ Search2DMatrix/Search2DMatrix .py
@@ -1,31 +1,30 @@
-
 from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # Verifica se a matriz ou a primeira linha são vazias
         if not matrix or not matrix[0]:
             return False
         
-        # Obtém o número de linhas (m) e colunas (n) da matriz
         m, n = len(matrix), len(matrix[0])
+        return self.searchSubmatrix(matrix, target, 0, 0, m - 1, n - 1)
+    
+    def searchSubmatrix(self, matrix: List[List[int]], target: int, startRow: int, startCol: int, endRow: int, endCol: int) -> bool:
+        if startRow > endRow or startCol > endCol:
+            return False
         
-        # Inicializa a posição de início na primeira linha e última coluna
-        row = 0
-        col = n - 1
+        midRow = (startRow + endRow) // 2
+        midCol = (startCol + endCol) // 2
+        midElement = matrix[midRow][midCol]
         
-        # Executa um loop enquanto a posição atual estiver dentro dos limites da matriz
-        while row < m and col >= 0:
-            # Verifica se o elemento atual é igual ao alvo
-            if matrix[row][col] == target:
-                return True
-            # Se o elemento atual for maior que o alvo, move para a coluna anterior
-            elif matrix[row][col] > target:
-                col -= 1
-            # Se o elemento atual for menor que o alvo, move para a próxima linha
-            else:
-                row += 1
-        
-        # Se o alvo não for encontrado na matriz, retorna False
-        return False
-
+        if midElement == target:
+            return True
+        elif midElement > target:
+            # Divide a submatriz em quatro partes menores e realiza a recursão em cada uma delas
+            return self.searchSubmatrix(matrix, target, startRow, startCol, midRow - 1, midCol - 1) or \
+                   self.searchSubmatrix(matrix, target, startRow, midCol, midRow - 1, endCol) or \
+                   self.searchSubmatrix(matrix, target, midRow, startCol, endRow, midCol - 1)
+        else:
+            # Divide a submatriz em quatro partes menores e realiza a recursão em cada uma delas
+            return self.searchSubmatrix(matrix, target, startRow, midCol + 1, midRow, endCol) or \
+                   self.searchSubmatrix(matrix, target, midRow + 1, startCol, endRow, midCol) or \
+                   self.searchSubmatrix(matrix, target, midRow + 1, midCol + 1, endRow, endCol)
